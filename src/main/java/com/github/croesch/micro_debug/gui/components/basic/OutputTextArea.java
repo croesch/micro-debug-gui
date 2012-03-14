6363fca04221bb6b886605129119e9966b4778ad
croesch <christianroesch@gmx.net>
2012-03-14 22:00:40 +0100

    #7 implemented basic output text area for the debugger

---
+++ src/main/java/com/github/croesch/micro_debug/gui/components/basic/OutputTextArea.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of micro-debug-gui.
+ * 
+ * micro-debug-gui is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * micro-debug-gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with micro-debug-gui.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.micro_debug.gui.components.basic;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.swing.SwingUtilities;
+
+import com.github.croesch.micro_debug.commons.Printer;
+
+/**
+ * Text area that can be used to view the output of the {@link Printer}.
+ * 
+ * @author croesch
+ * @since Date: Mar 14, 2012
+ */
+public class OutputTextArea extends MDTextArea {
+
+  /** generated serial version UID */
+  private static final long serialVersionUID = -1970161406114775052L;
+
+  /** the {@link PrintStream} that is able to append text to this text area */
+  private final transient PrintStream stream;
+
+  /**
+   * Constructs a new text area, without activating it. To see the output of the {@link Printer} see {@link #activate()}
+   * .
+   * 
+   * @since Date: Mar 14, 2012
+   * @param name the name of this text area
+   * @see #activate()
+   */
+  public OutputTextArea(final String name) {
+    super(name);
+    setEditable(false);
+
+    this.stream = new PrintStream(new OutputStream() {
+      @Override
+      public void write(final int b) throws IOException {
+        SwingUtilities.invokeLater(new Runnable() {
+          public void run() {
+            OutputTextArea.this.append(String.valueOf((char) b));
+          }
+        });
+      }
+    });
+  }
+
+  /**
+   * Informs the {@link Printer} to print its output onto this text area.
+   * 
+   * @since Date: Mar 14, 2012
+   */
+  public final void activate() {
+    Printer.setPrintStream(this.stream);
+  }
+
+  /**
+   * Deletes the text of this text area.
+   * 
+   * @since Date: Mar 14, 2012
+   */
+  public final void reset() {
+    setText(null);
+  }
+}
---
+++ src/test/java/com/github/croesch/micro_debug/gui/components/basic/OutputTextAreaTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of micro-debug-gui.
+ * 
+ * micro-debug-gui is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * micro-debug-gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with micro-debug-gui.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.micro_debug.gui.components.basic;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.edt.GuiTask;
+import org.fest.swing.fixture.JTextComponentFixture;
+import org.junit.Test;
+
+import com.github.croesch.micro_debug.commons.Printer;
+import com.github.croesch.micro_debug.gui.DefaultGUITestCase;
+
+/**
+ * Provides some test cases for {@link OutputTextArea}.
+ * 
+ * @author croesch
+ * @since Date: Mar 14, 2012
+ */
+public class OutputTextAreaTest extends DefaultGUITestCase {
+
+  private OutputTextArea getTA(final String name) {
+    return GuiActionRunner.execute(new GuiQuery<OutputTextArea>() {
+      @Override
+      protected OutputTextArea executeInEDT() {
+        return new OutputTextArea(name);
+      }
+    });
+  }
+
+  @Test
+  public void testTextArea() throws InterruptedException, InvocationTargetException {
+    printlnMethodName();
+    final JTextComponentFixture taFixture = new JTextComponentFixture(robot(), getTA("ta"));
+    taFixture.requireNotEditable();
+    taFixture.requireEmpty();
+    assertThat(taFixture.component().getName()).isEqualTo("ta");
+
+    Printer.println("not printed line ...");
+    taFixture.requireEmpty();
+    assertThat(out.toString()).isEqualTo("not printed line ..." + getLineSeparator());
+    out.reset();
+
+    activate(taFixture);
+
+    Printer.println("printed line ...");
+    taFixture.requireText("printed line ..." + getLineSeparator());
+    assertThat(out.toString()).isEmpty();
+
+    reset(taFixture);
+    taFixture.requireEmpty();
+
+    SwingUtilities.invokeAndWait(new Runnable() {
+      public void run() {
+        Printer.println("printed another line ...");
+      }
+    });
+    taFixture.requireText("printed another line ..." + getLineSeparator());
+    assertThat(out.toString()).isEmpty();
+
+    Printer.println("printed line ...");
+    taFixture.requireText("printed another line ..." + getLineSeparator() + "printed line ..." + getLineSeparator());
+    assertThat(out.toString()).isEmpty();
+  }
+
+  private void activate(final JTextComponentFixture taFixture) {
+    GuiActionRunner.execute(new GuiTask() {
+      @Override
+      protected void executeInEDT() throws Throwable {
+        taFixture.targetCastedTo(OutputTextArea.class).activate();
+      }
+    });
+  }
+
+  private void reset(final JTextComponentFixture taFixture) {
+    GuiActionRunner.execute(new GuiTask() {
+      @Override
+      protected void executeInEDT() throws Throwable {
+        taFixture.targetCastedTo(OutputTextArea.class).reset();
+      }
+    });
+  }
+}
