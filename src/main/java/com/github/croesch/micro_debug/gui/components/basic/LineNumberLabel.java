5777c1f24664ae092a8e8ed32ac5ad13acdd3882
croesch <christianroesch@gmx.net>
2012-03-20 00:28:55 +0100

    #8 implemented component to visualising line numbers

---
+++ src/main/java/com/github/croesch/micro_debug/gui/components/basic/LineNumberLabel.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of micro-debug-gui.
+ * 
+ * micro-debug-gui is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * micro-debug-gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with micro-debug-gui.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.micro_debug.gui.components.basic;
+
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.JTextComponent;
+
+import com.github.croesch.micro_debug.commons.Utils;
+
+/**
+ * Vertical row view that shows line numbers for a given text component.
+ * 
+ * @author croesch
+ * @since Date: Mar 19, 2012
+ */
+public class LineNumberLabel extends MDLabel implements DocumentListener {
+
+  /** mask to select the bits for representing a html-code color */
+  private static final int HTML_COLOR_CODE_MASK = 0xFFFFFF;
+
+  /** generated serial version UID */
+  private static final long serialVersionUID = -5234458420083482975L;
+
+  /** the string that represents the HTML-Code for the color to highlight a specific line */
+  private final String highColor;
+
+  /** the number of the line that is highlighted */
+  private int highlightedLine = -1;
+
+  /** the text component this component shows the line numbers for */
+  private final JTextComponent textArea;
+
+  /**
+   * Constructs this ruler that shows line numbers for the given component. Will update itself, when the underlying
+   * document of the given text component changes.
+   * 
+   * @since Date: Mar 20, 2012
+   * @param ta the text component to show line numbers for, must not be <code>null</code>.
+   */
+  public LineNumberLabel(final JTextComponent ta) {
+    super(ta.getName() + "-line-numbers", null);
+    setVerticalAlignment(SwingConstants.TOP);
+
+    this.highColor = "#"
+                     + Integer.toHexString(UIManager.getColor("Label.background").darker().getRGB()
+                                           & HTML_COLOR_CODE_MASK);
+
+    this.textArea = ta;
+    this.textArea.getDocument().addDocumentListener(this);
+    update();
+  }
+
+  /**
+   * Returns the current line numbers of this text component. Based on {@link javax.swing.JTextArea#getLineCount()}.
+   * 
+   * @since Date: Mar 20, 2012
+   * @return the number of lines the underlying text component currently has
+   */
+  private int getLineCount() {
+    return this.textArea.getDocument().getDefaultRootElement().getElementCount();
+  }
+
+  /**
+   * Updates the line numbers.<br>
+   * <i>Caution:</i> Do use carefully! Will rebuild the line numbers completely, which is not performant - especially
+   * for high line count.
+   * 
+   * @since Date: Mar 20, 2012
+   */
+  private void update() {
+    final StringBuilder sb = new StringBuilder("<html>");
+
+    for (int line = 1; line <= getLineCount(); ++line) {
+      if (line == this.highlightedLine) {
+        // insert color information for highlighted line
+        sb.append("<font bgcolor='").append(this.highColor).append("'>");
+      }
+
+      sb.append(Utils.toHexString(line));
+
+      if (line == this.highlightedLine) {
+        // end color information for highlighted line
+        sb.append("</font>");
+      }
+      sb.append("<br>");
+    }
+
+    setText(sb.toString());
+  }
+
+  /**
+   * Changes the highlight to the given line. If the given line doesn't exist, nothing will be highlighted.
+   * <i>Caution:</i> Do use carefully! Will rebuild the line numbers completely, which is not performant - especially
+   * for high line count.
+   * 
+   * @since Date: Mar 20, 2012
+   * @param line the number of the line to highlight
+   */
+  public final void highlight(final int line) {
+    this.highlightedLine = line;
+    update();
+  }
+
+  /**
+   * Gives notification that there was an insert into the document. The range given by the DocumentEvent bounds the
+   * freshly inserted region.
+   * 
+   * @param e the document event
+   */
+  public final void insertUpdate(final DocumentEvent e) {
+    update();
+  }
+
+  /**
+   * Gives notification that a portion of the document has been removed. The range is given in terms of what the view
+   * last saw (that is, before updating sticky positions).
+   * 
+   * @param e the document event
+   */
+  public final void removeUpdate(final DocumentEvent e) {
+    update();
+  }
+
+  /**
+   * Gives notification that an attribute or set of attributes changed.
+   * 
+   * @param e the document event
+   */
+  public final void changedUpdate(final DocumentEvent e) {
+    // nothing to do
+  }
+}
---
+++ src/test/java/com/github/croesch/micro_debug/gui/components/basic/LineNumberLabelTest.java
@@ -0,0 +1,295 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of micro-debug-gui.
+ * 
+ * micro-debug-gui is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * micro-debug-gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with micro-debug-gui.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.micro_debug.gui.components.basic;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JComponent;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.text.JTextComponent;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.edt.GuiTask;
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JLabelFixture;
+import org.fest.swing.fixture.JTextComponentFixture;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.github.croesch.micro_debug.commons.Utils;
+import com.github.croesch.micro_debug.gui.DefaultGUITestCase;
+
+/**
+ * Provides test cases for {@link LineNumberLabel}.
+ * 
+ * @author croesch
+ * @since Date: Mar 19, 2012
+ */
+public class LineNumberLabelTest extends DefaultGUITestCase {
+
+  private static final String HIGHLIGHT = Integer
+    .toHexString(UIManager.getColor("Label.background").darker().getRGB() & 0xFFFFFF);
+
+  private LineNumberLabel getLabel(final JTextComponent ta) {
+    return GuiActionRunner.execute(new GuiQuery<LineNumberLabel>() {
+      @Override
+      protected LineNumberLabel executeInEDT() {
+        return new LineNumberLabel(ta);
+      }
+    });
+  }
+
+  private MDTextArea getTA(final String name, final String text) {
+    return GuiActionRunner.execute(new GuiQuery<MDTextArea>() {
+      @Override
+      protected MDTextArea executeInEDT() {
+        return new MDTextArea(name, text);
+      }
+    });
+  }
+
+  private JEditorPane getEP(final String name, final String text) {
+    return GuiActionRunner.execute(new GuiQuery<JEditorPane>() {
+      @Override
+      protected JEditorPane executeInEDT() {
+        final JEditorPane pane = new JEditorPane();
+        pane.setName(name);
+        pane.setText(text);
+        return pane;
+      }
+    });
+  }
+
+  private JFrame getFrame(final JComponent comp, final JLabel label) {
+    return GuiActionRunner.execute(new GuiQuery<JFrame>() {
+      @Override
+      protected JFrame executeInEDT() {
+        final JFrame frame = new JFrame();
+        frame.setLayout(new MigLayout("fill", "[][grow,fill]", "[grow,fill]"));
+        frame.add(label);
+        frame.add(comp);
+        return frame;
+      }
+    });
+  }
+
+  @Test
+  public void testLabel() {
+    printlnMethodName();
+    JTextArea ta = getTA("ta", "Dies ist ein Text ...");
+    JLabelFixture labelFixture = new JLabelFixture(robot(), getLabel(ta));
+    labelFixture.requireText(getTextAsserted(1));
+    assertThat(labelFixture.component().getName()).isEqualTo("ta-line-numbers");
+
+    ta = getTA("", "");
+    labelFixture = new JLabelFixture(robot(), getLabel(ta));
+    labelFixture.requireText(getTextAsserted(1));
+    assertThat(labelFixture.component().getName()).isEqualTo("-line-numbers");
+
+    ta = getTA("ta", "\n");
+    labelFixture = new JLabelFixture(robot(), getLabel(ta));
+    labelFixture.requireText(getTextAsserted(2));
+    assertThat(labelFixture.component().getName()).isEqualTo("ta-line-numbers");
+
+    ta = getTA("ta", "\r\n");
+    labelFixture = new JLabelFixture(robot(), getLabel(ta));
+    labelFixture.requireText(getTextAsserted(2));
+    assertThat(labelFixture.component().getName()).isEqualTo("ta-line-numbers");
+
+    ta = getTA("ta", getLineSeparator() + "..\n\nvierte Zeile\n");
+    labelFixture = new JLabelFixture(robot(), getLabel(ta));
+    labelFixture.requireText(getTextAsserted(5));
+    assertThat(labelFixture.component().getName()).isEqualTo("ta-line-numbers");
+
+    ta = getTA(null, "");
+    labelFixture = new JLabelFixture(robot(), getLabel(ta));
+    labelFixture.requireText(getTextAsserted(1));
+    assertThat(labelFixture.component().getName()).isEqualTo("null-line-numbers");
+
+    ta = getTA("", null);
+    labelFixture = new JLabelFixture(robot(), getLabel(ta));
+    labelFixture.requireText(getTextAsserted(1));
+    assertThat(labelFixture.component().getName()).isEqualTo("-line-numbers");
+
+    final JEditorPane pane = getEP("ta", getLineSeparator() + "..\n\r\nvierte Zeile\n");
+    labelFixture = new JLabelFixture(robot(), getLabel(pane));
+    labelFixture.requireText(getTextAsserted(5));
+    assertThat(labelFixture.component().getName()).isEqualTo("ta-line-numbers");
+
+    // nothing happens
+    labelFixture.targetCastedTo(LineNumberLabel.class).changedUpdate(null);
+  }
+
+  @Test
+  public void testLabelEnterTextInTA() {
+    printlnMethodName();
+    final JTextComponentFixture ta = new JTextComponentFixture(robot(), getTA("ta", ""));
+    final JLabelFixture labelFixture = new JLabelFixture(robot(), getLabel(ta.targetCastedTo(JTextArea.class)));
+
+    final FrameFixture frame = new FrameFixture(robot(), getFrame(ta.component(), labelFixture.component()));
+    frame.show(new Dimension(500, 500));
+
+    labelFixture.requireText(getTextAsserted(1));
+    assertThat(labelFixture.component().getName()).isEqualTo("ta-line-numbers");
+
+    ta.enterText("1 2 3 4");
+    labelFixture.requireText(getTextAsserted(1));
+
+    ta.selectAll();
+    ta.enterText(" 5 6 7 8 9 10");
+    labelFixture.requireText(getTextAsserted(1));
+
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(2));
+
+    ta.enterText("zweite Zeile");
+    labelFixture.requireText(getTextAsserted(2));
+
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(3));
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(4));
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(5));
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(6));
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(7));
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(8));
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(9));
+    ta.enterText("\n");
+    labelFixture.requireText(getTextAsserted(10));
+    ta.enterText("hihi");
+    labelFixture.requireText(getTextAsserted(10));
+  }
+
+  @Test
+  public void testHighlight() {
+    printlnMethodName();
+    final JTextComponentFixture ta = new JTextComponentFixture(robot(), getTA("light", "Dies ist ein Text ..."));
+    final JLabelFixture labelFixture = new JLabelFixture(robot(), getLabel(ta.targetCastedTo(JTextArea.class)));
+
+    final FrameFixture frame = new FrameFixture(robot(), getFrame(ta.component(), labelFixture.component()));
+    frame.show(new Dimension(500, 500));
+    assertThat(labelFixture.component().getName()).isEqualTo("light-line-numbers");
+
+    // assert highlight has no effect
+    labelFixture.requireText(getTextAsserted(1));
+    highlight(labelFixture, -10);
+    labelFixture.requireText(getTextAsserted(1));
+    highlight(labelFixture, -1);
+    labelFixture.requireText(getTextAsserted(1));
+    highlight(labelFixture, 0);
+    labelFixture.requireText(getTextAsserted(1));
+    highlight(labelFixture, 2);
+    labelFixture.requireText(getTextAsserted(1));
+    highlight(labelFixture, 10);
+    labelFixture.requireText(getTextAsserted(1));
+
+    highlight(labelFixture, 1);
+    labelFixture.requireText(getTextAsserted(1, 1));
+
+    ta.deleteText();
+    labelFixture.requireText(getTextAsserted(1, 1));
+    ta.enterText(getLineSeparator() + "..\r\n\nvierte Zeile\n");
+    labelFixture.requireText(getTextAsserted(5, 1));
+
+    highlight(labelFixture, 3);
+    labelFixture.requireText(getTextAsserted(5, 3));
+
+    highlight(labelFixture, 6);
+    labelFixture.requireText(getTextAsserted(5));
+
+    highlight(labelFixture, 5);
+    labelFixture.requireText(getTextAsserted(5, 5));
+
+    ta.selectText(0, getLineSeparator().length());
+    ta.pressAndReleaseKeys(KeyEvent.VK_DELETE);
+    labelFixture.requireText(getTextAsserted(4));
+  }
+
+  @Test
+  @Ignore("just for manual testing purpose to become a feeling of how long things take")
+  public void testHighlightPerformance() {
+    printlnMethodName();
+    final JTextComponentFixture ta = new JTextComponentFixture(robot(), getTA("light", "Dies ist ein Text ..."));
+    final JLabelFixture labelFixture = new JLabelFixture(robot(), getLabel(ta.targetCastedTo(JTextArea.class)));
+
+    final FrameFixture frame = new FrameFixture(robot(), getFrame(ta.component(), labelFixture.component()));
+    frame.show(new Dimension(500, 500));
+
+    final StringBuilder sb = new StringBuilder();
+    for (int i = 1; i < 100; ++i) {
+      sb.append(i).append(" ...\n");
+    }
+
+    ta.setText(sb.toString());
+
+    for (int i = 0; i < 100; ++i) {
+      highlight(labelFixture, i);
+      labelFixture.requireText(getTextAsserted(100, i));
+    }
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testNullTA() {
+    new JLabelFixture(robot(), getLabel(null));
+  }
+
+  private String getTextAsserted(final int lines) {
+    final StringBuilder sb = new StringBuilder("<html>");
+    for (int line = 1; line <= lines; ++line) {
+      sb.append(Utils.toHexString(line)).append("<br>");
+    }
+    return sb.toString();
+  }
+
+  private String getTextAsserted(final int lines, final int highlighted) {
+    final String toReplaceSuffix = Utils.toHexString(highlighted) + "<br>";
+    final String replacementSuffix = "<font bgcolor='#" + HIGHLIGHT + "'>" + Utils.toHexString(highlighted)
+                                     + "</font><br>";
+    switch (highlighted) {
+      case 1:
+        return getTextAsserted(lines).replace("<html>" + toReplaceSuffix, "<html>" + replacementSuffix);
+      default:
+        return getTextAsserted(lines).replace("<br>" + toReplaceSuffix, "<br>" + replacementSuffix);
+    }
+  }
+
+  private void highlight(final JLabelFixture labelFixture, final int line) {
+    GuiActionRunner.execute(new GuiTask() {
+      @Override
+      protected void executeInEDT() throws Throwable {
+        labelFixture.targetCastedTo(LineNumberLabel.class).highlight(line);
+      }
+    });
+  }
+}
